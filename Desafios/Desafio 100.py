67bae36169922fe2168505393cc47b1bab50c39b
tiagosm1 <72826762+tiagosm1@users.noreply.github.com>
2020-12-03 14:11:38 -0300

    ex100

--- Desafios/Desafio 100.py
+++ Desafios/Desafio 100.py
@@ -1,1 +1,24 @@
+from random import randint
+from time import sleep
 
+
+def sorteia(lista):
+    print('Sorteando valroes da lista:', end='')
+    for cont in range(0, 5):
+        n = randint(1, 10)
+        lista.append(n)
+        print(f'{n} ', end='', flush=True)
+        sleep(0.3)
+    print('Pronto')
+
+
+def somapar(lista):
+    soma = 0
+    for valor in lista:
+        if valor % 2 == 0:
+            soma += valor
+    print(f'Somando os valores pares de {lista}, temos a {soma}')
+    
+numeros = list()
+sorteia(numeros)
+somapar(numeros)
