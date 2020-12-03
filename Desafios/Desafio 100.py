4715715f9fe93816d62b790fd9fdf9ffdbdbd9a0
tiagosm1 <72826762+tiagosm1@users.noreply.github.com>
2020-12-03 16:57:01 -0300

    Update Desafio 100.py

--- Desafios/Desafio 100.py
+++ Desafios/Desafio 100.py
@@ -3,8 +3,8 @@ from time import sleep
 
 
 def sorteia(lista):
-    print('Sorteando valroes da lista:', end='')
-    for cont in range(0, 5):
+    print('Sorteando valores da lista:', end='')
+    for c in range(0, 5):
         n = randint(1, 10)
         lista.append(n)
         print(f'{n} ', end='', flush=True)
