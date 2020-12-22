06f9738825a7d2653a8b4987efc3729f16790aa4
TIAGO MOREIRA <72826762+tiagosm1@users.noreply.github.com>
2020-12-22 10:54:44 -0300

    105

--- Desafios/Desafio 104.py
+++ Desafios/Desafio 104.py
@@ -1,1 +1,13 @@
-n = leiaInt('Digite um número: ')
+def leiaint(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[0;31mErro! Digite um número inteiro válido!\033[m')
+
+n = leiaint('Digite um número: ')
+print(f'Você acabou de digitar o número {n}')
